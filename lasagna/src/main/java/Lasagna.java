de18291c5b51a42b105fb82e2aad1aef452ff322
purplevarun <purplevarun@gmail.com>
2022-01-07 12:50:30 +0530

    lasanga done

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,21 +1,21 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
-    int expectedMinutesInOven(){
+    public int expectedMinutesInOven(){
         return 40;
     }
 
     // TODO: define the 'remainingMinutesInOven()' method
-    int remainingMinutesInOven(int n){
+    public int remainingMinutesInOven(int n){
         return this.expectedMinutesInOven() - n;
     }
 
     // TODO: define the 'preparationTimeInMinutes()' method
-    int preparationTimeInMinutes(int n){
+    public int preparationTimeInMinutes(int n){
         return n*2;
     }
 
     // TODO: define the 'totalTimeInMinutes()' method
-    int totalTimeInMinutes(int n, int m){
-        return this.preparationTimeInMinutes(n) + this.remainingMinutesInOven(m);
+    public int totalTimeInMinutes(int n, int m){
+        return this.preparationTimeInMinutes(n) + m;
     }
 }
