6f3d95a08b509778428774aa767a18f5a77d5275
purplevarun <purplevarun@gmail.com>
2022-01-09 13:25:58 +0530

    red hat

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class PythagoreanTriplet{
     private int a, b, c;
+    private int n;
     PythagoreanTriplet (int a, int b, int c){
         int [] arr = {a, b, c};
         Arrays.sort (arr);
@@ -8,7 +9,21 @@ class PythagoreanTriplet{
         this.b = arr[1];
         this.c = arr[2];
     }
+    void makeTripletsList(){
+        return;
+    }
+    void withFactorsLessThanOrEqualTo(int n){
+        return;
+    }
+    void thatSumTo(int n){
+        this.n = n;
+    }
+    
     public static void main (String args []){
-        System.out.println(new PythagoreanTriplet(4,2,1).a);
+        // System.out.println(new PythagoreanTriplet(4,2,1).a);
+        List<PythagoreanTriplet> expected
+                = Collections.singletonList(new PythagoreanTriplet(200, 375, 425));
+
+        System.out.println(expected);
     }
 }
